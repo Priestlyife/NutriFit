19af3d0054ccca0db71c74d1413e75742e93d459
Akinmulero Priestly <PA724@live.mdx.ac.uk>
2026-04-01 12:08:51 +0400

    Clean commit without secrets

--- server/index.js
+++ server/index.js
@@ -39,3 +39,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.get("/api", (req, res) => {
+  res.json({ message: "API is working 🚀" });
+});
